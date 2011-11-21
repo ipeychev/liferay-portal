ec316db4e2cc1099d71c12af67f8b250d79be980
Iliyan Peychev <iliyan.peychev@liferay.com>
2011-11-21 10:57:29 +0100

    LPS-17831 - Add multiple requests support to DML

--- portal-web/docroot/html/portlet/document_library/js/main.js
+++ portal-web/docroot/html/portlet/document_library/js/main.js
@@ -262,9 +262,20 @@ AUI().add(
 							instance._addHistoryState(data);
 						}
 
-						var ioRequest = instance._getIORequest();
+						var ioRequest = A.io.request(
+							instance._config.mainUrl,
+							{
+								autoLoad: false
+							}
+						);
+
+						var sendIOResponse = A.bind(instance._sendIOResponse, instance, ioRequest);
+
+						ioRequest.after(['failure', 'success'], sendIOResponse);
 
 						ioRequest.set(STR_DATA, data);
+
+						instance._lastRequestData = data;
 
 						ioRequest.start();
 					},
@@ -332,9 +343,9 @@ AUI().add(
 						var requestParams = {};
 
 						requestParams[instance.ns(STRUTS_ACTION)] = config.strutsAction;
-						requestParams[instance.ns(STR_ENTRY_END)] = config.entryRowsPerPage || instance._entryPaginator.get('rowsPerPage');
+						requestParams[instance.ns(STR_ENTRY_END)] = config.entryRowsPerPage || instance._entryPaginator.get(ROWS_PER_PAGE);
 						requestParams[instance.ns(STR_ENTRY_START)] = 0;
-						requestParams[instance.ns(STR_FOLDER_END)] = config.folderRowsPerPage || instance._folderPaginator.get('rowsPerPage');
+						requestParams[instance.ns(STR_FOLDER_END)] = config.folderRowsPerPage || instance._folderPaginator.get(ROWS_PER_PAGE);
 						requestParams[instance.ns(STR_FOLDER_START)] = 0;
 						requestParams[instance.ns('refreshEntries')] = dataRefreshEntries;
 						requestParams[instance.ns(VIEW_ADD_BUTTON)] = true;
@@ -391,31 +402,6 @@ AUI().add(
 						return displayStyle;
 					},
 
-					_getIORequest: function() {
-						var instance = this;
-
-						var ioRequest = instance._ioRequest;
-
-						if (!ioRequest) {
-							var sendIOResponse = A.bind(instance._sendIOResponse, instance);
-
-							ioRequest = A.io.request(
-								instance._config.mainUrl,
-								{
-									after: {
-										success: sendIOResponse,
-										failure: sendIOResponse
-									},
-									autoLoad: false
-								}
-							);
-
-							instance._ioRequest = ioRequest;
-						}
-
-						return ioRequest;
-					},
-
 					_getMoveText: function(selectedItemsCount, targetAvailable) {
 						var moveText = STR_BLANK;
 
@@ -441,7 +427,7 @@ AUI().add(
 						var instance = this;
 
 						if (!Lang.isValue(page)) {
-							var page = 0;
+							page = 0;
 
 							var curPage = paginator.get('page') - 1;
 
@@ -756,8 +742,8 @@ AUI().add(
 						var instance = this;
 
 						var startEndParams = instance._getResultsStartEnd(instance._entryPaginator);
-
-						var requestParams = instance._getIORequest().get(STR_DATA) || {};
+						
+						var requestParams = instance._lastRequestData || {};
 
 						var customParams = {};
 
@@ -783,7 +769,7 @@ AUI().add(
 
 						var startEndParams = instance._getResultsStartEnd(instance._folderPaginator);
 
-						var requestParams = instance._getIORequest().get(STR_DATA) || {};
+						var requestParams = instance._lastRequestData || {};
 
 						var customParams = {};
 
@@ -984,22 +970,20 @@ AUI().add(
 
 						var searchInfo = instance.one('#searchInfo', content);
 
+						var entriesContainer = instance._entriesContainer;
+
+						entriesContainer.plug(A.Plugin.ParseContent);
+
 						if (searchInfo) {
-							var entriesContainer = instance._entriesContainer;
-
-							entriesContainer.plug(A.Plugin.ParseContent);
-
 							entriesContainer.setContent(searchInfo);
 						}
 
 						var searchResults = instance.one('.local-search-results', content);
 
+						var repositorySearchResultsContainer;
+
 						if (searchResults) {
-							var repositorySearchResultsContainer = instance.one('#' + instance.ns('searchResultsContainer'), content);
-
-							var entriesContainer = instance._entriesContainer;
-
-							entriesContainer.plug(A.Plugin.ParseContent);
+							repositorySearchResultsContainer = instance.one('#' + instance.ns('searchResultsContainer'), content);
 
 							entriesContainer.append(repositorySearchResultsContainer);
 						}
@@ -1009,9 +993,7 @@ AUI().add(
 						if (repositorySearchResults) {
 							var repositoryId = instance.one('#' + instance.ns('repositoryId'), content);
 
-							var entriesContainer = instance._entriesContainer;
-
-							var repositorySearchResultsContainer = entriesContainer.one('#' + instance.ns('repositorySearchResultsContainer') + repositoryId.val());
+							repositorySearchResultsContainer = entriesContainer.one('#' + instance.ns('repositorySearchResultsContainer') + repositoryId.val());
 
 							repositorySearchResultsContainer.plug(A.Plugin.ParseContent);
 
@@ -1019,10 +1001,8 @@ AUI().add(
 						}
 					},
 
-					_sendIOResponse: function(event) {
+					_sendIOResponse: function(ioRequest, event) {
 						var instance = this;
-
-						var ioRequest = instance._getIORequest();
 
 						var data = ioRequest.get(STR_DATA);
 						var reponseData = ioRequest.get('responseData');
