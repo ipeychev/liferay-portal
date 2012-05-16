eb374ec22e8b211a7e43fb99027918a6da83bcd8
Levente Hudak <levente.hudak@liferay.com>
2012-05-16 11:25:37 +0200

    LPS-27029 Implement Search for the Recycle Bin.

--- portal-impl/src/com/liferay/portlet/blogs/service/impl/BlogsEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/blogs/service/impl/BlogsEntryLocalServiceImpl.java
@@ -203,6 +203,15 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 			user.getCompanyId(), groupId, userId, BlogsEntry.class.getName(),
 			entry.getEntryId(), entry, serviceContext);
 
+		// Indexer
+
+		if (entry.getStatus() == WorkflowConstants.STATUS_DRAFT) {
+			Indexer indexer = IndexerRegistryUtil.nullSafeGetIndexer(
+				BlogsEntry.class);
+
+			indexer.reindex(entry);
+		}
+
 		return entry;
 	}
 
@@ -817,6 +826,15 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 			entryId, SocialActivityConstants.TYPE_RESTORE_FROM_TRASH,
 			StringPool.BLANK, 0);
 
+		//Indexer
+
+		Indexer indexer = IndexerRegistryUtil.nullSafeGetIndexer(
+			BlogsEntry.class);
+
+		BlogsEntry entry = getBlogsEntry(entryId);
+
+		indexer.reindex(entry);
+
 		// Trash
 
 		trashEntryLocalService.deleteEntry(trashEntry.getEntryId());
@@ -1075,15 +1093,23 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 			if (status == WorkflowConstants.STATUS_IN_TRASH) {
 				assetEntryLocalService.moveEntryToTrash(
 					BlogsEntry.class.getName(), entryId);
+
+				entry.setGroupId(-entry.getGroupId());
+				entry.setStatus(oldStatus);
+
+				indexer.reindex(entry);
+
+				entry.setGroupId(-entry.getGroupId());
+				entry.setStatus(status);
 			}
 			else {
 				assetEntryLocalService.updateVisible(
 					BlogsEntry.class.getName(), entryId, false);
+
+				// Indexer
+
+				indexer.delete(entry);
 			}
-
-			// Indexer
-
-			indexer.delete(entry);
 		}
 
 		return entry;
