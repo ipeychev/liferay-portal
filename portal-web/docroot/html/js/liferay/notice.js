b11db24313ac043beee8dd51f32433dd7f1e0881
Chema Balsas <jose.balsas@liferay.com>
2013-11-06 15:47:38 +0100

    LPS-42070 Toggle notice visibility to prevent it to show on resizing

--- portal-web/docroot/html/js/liferay/notice.js
+++ portal-web/docroot/html/js/liferay/notice.js
@@ -134,6 +134,12 @@ AUI.add(
 				}
 			},
 
+			_beforeNoticeShow: function(event) {
+				var instance = this;
+
+				instance._notice.toggle(true);
+			},
+
 			_afterNoticeShow: function(event) {
 				var instance = this;
 
@@ -194,7 +200,12 @@ AUI.add(
 						}
 					);
 
-					instance._notice.transition(animationConfig);
+					instance._notice.transition(
+						animationConfig,
+						function() {
+							instance._notice.toggle(false);
+						}
+					);
 
 					returnVal = new Do.Halt(null);
 				}
@@ -239,6 +250,8 @@ AUI.add(
 				instance._body.addClass(CSS_ALERTS);
 
 				Do.before(instance._beforeNoticeHide, notice, STR_HIDE, instance);
+
+				Do.before(instance._beforeNoticeShow, notice, STR_SHOW, instance);
 
 				Do.after(instance._afterNoticeShow, notice, STR_SHOW, instance);
 
