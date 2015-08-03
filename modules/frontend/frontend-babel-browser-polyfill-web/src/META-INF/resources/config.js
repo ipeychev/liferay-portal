e49aa3bb9c2fff75566d4731dd176ff7e3f84fe0
Chema Balsas <jose.balsas@liferay.com>
2015-08-03 18:25:27 +0200

    LPS-57615 Create Babel Browser Polyfill OSGi module

---
+++ modules/frontend/frontend-babel-browser-polyfill-web/src/META-INF/resources/config.js
@@ -0,0 +1,8 @@
+Loader.addModule(
+	{
+		dependencies: [],
+		exports: '_babelPolyfill',
+		name: 'babel-browser-polyfill',
+		path: '/o/frontend-babel-browser-polyfill/browser-polyfill.min.js'
+	}
+);
